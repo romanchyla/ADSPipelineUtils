c11f6558fe4f3c4ecc6d0a4faf629e4382755a9b
Roman Chyla <roman.chyla@gmail.com>
2017-06-16 20:44:36 -0400

    Adding support for serialized Protobufs

---
+++ adsputils/tests/test_serializer.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+import sys
+import os
+
+import unittest
+import json
+import inspect
+from mock import patch
+from kombu import serialization
+from adsputils import serializer
+from adsmsg import BibRecord
+from adsmsg.msg import Msg
+
+class TestAdsOrcidCelery(unittest.TestCase):
+    """
+    Tests the JSON serializer of adsmsg protobufs
+    """
+    
+    def setUp(self):
+        unittest.TestCase.setUp(self)
+        serialization.register('adsmsg', *serializer.register_args)
+    
+    
+    def tearDown(self):
+        unittest.TestCase.tearDown(self)
+        serialization.unregister('adsmsg')
+        
+    
+    def xtest_serializer(self):
+        b = BibRecord(bibcode='bibcode')
+        cls, data = b.dump()
+        
+        self.assertEqual('adsmsg.bibrecord.BibRecord', cls)
+        self.assertEqual(data, '\n\x07bibcode') # let's hope the upstream adsmsg doesn't change impl...
+        
+        
+        # dump using default 'json' serializer
+        content_type, encoding, data = serialization.dumps(b)
+        self.assertEqual('{"__adsmsg__": ["adsmsg.bibrecord.BibRecord", "\\n\\u0007bibcode"]}',
+                         data)
+        
+        # load using default 'json' deserializer
+        self.assertEqual(serialization.loads(data, content_type, encoding),
+                         {u'__adsmsg__': [u'adsmsg.bibrecord.BibRecord', u'\n\x07bibcode']})
+        
+        # load using our deserializer
+        o = serialization.loads(data, 'application/x-adsmsg', encoding)
+        self.assertTrue(isinstance(o, BibRecord))
+        self.assertEqual(o.bibcode, 'bibcode')
+
+        
+        # and arbitrarily nested objects should pose no difficulties
+        test = [{'foo': b}, b, [[b]]]
+        content_type, encoding, data = serialization.dumps(test)
+        o = serialization.loads(data, 'application/x-adsmsg', encoding)
+        
+        for x in (o[0]['foo'], o[1], o[2][0][0]):
+            self.assertTrue(isinstance(x, BibRecord))
+            self.assertEqual(x.bibcode, "bibcode")
+    
+    def test_utf8(self):
+        b = BibRecord(bibcode=u'\u01b5')
+        ctype, enc, data = serialization.dumps(b)
+        o = serialization.loads(data, 'application/x-adsmsg', 'utf-8')
+        self.assertTrue(isinstance(o, BibRecord))
+        self.assertEqual(o.bibcode, u'\u01b5')
+        
+
+if __name__ == '__main__':
+    unittest.main()
--- setup.py
+++ setup.py
@@ -1,12 +1,25 @@
 from setuptools import setup
 import os
 
+
+# hack: we can't use github in the install_requires section; so until we have an official 
+# release of ADSPipelineMsg package available, this has to suffice... 
+os.system('pip install --upgrade git+https://github.com/adsabs/ADSPipelineMsg.git@master')
+
 setup(name='adsputils',
       version='0.0.1',
       packages=['adsputils'],
       install_requires=[
           'ConcurrentLogHandler==0.9.1',
           'python-dateutil==2.6.0',
-          'DateTime==4.1.1'
-      ]
+          'DateTime==4.1.1',
+          'celery==4.0.2',
+          #'ADSPipelineMsg==1.0.0'
+          
+      ],
+      entry_points={
+            'kombu.serializers': [
+                'adsmsg = adsputils.serializer:register_args'
+            ]
+        }
   )
